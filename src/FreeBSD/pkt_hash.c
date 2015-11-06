85bfbd41fb3d9b80242fcbe05e263266a59c6d21
Asim Jamshed <ajamshed@ndsl.kaist.edu>
2015-11-05 20:17:53 -0800

    - Made corrections to the README file - Added FreeBSD toeplitz library functions 	--> Currently testing..

--- src/FreeBSD/pkt_hash.c
+++ src/FreeBSD/pkt_hash.c
@@ -43,6 +43,196 @@
 #include <netinet/udp.h>
 /* eth hdr */
 #include <net/ethernet.h>
+/* for memset */
+#include <string.h>
+/*---------------------------------------------------------------------*/
+/* no. of bits in a byte */
+#define NBBY			8
+#define TOEPLITZ_KEYLEN_MAX     40
+#define TOEPLITZ_KEYSEED_CNT    2
+#define TOEPLITZ_KEYSEED0       0x6d
+#define TOEPLITZ_KEYSEED1       0x5a
+#define TOEPLITZ_INIT_KEYLEN    (TOEPLITZ_KEYSEED_CNT + sizeof(uint32_t))
+
+static uint32_t toeplitz_keyseeds[TOEPLITZ_KEYSEED_CNT] =
+	{ TOEPLITZ_KEYSEED0, TOEPLITZ_KEYSEED1 };
+
+uint32_t        toeplitz_cache[TOEPLITZ_KEYSEED_CNT][256];
+/*---------------------------------------------------------------------*/
+static inline uint32_t __unused
+toeplitz_rawhash_addrport(in_addr_t _faddr, in_addr_t _laddr,
+			  in_port_t _fport, in_port_t _lport)
+{
+	TRACE_PKTHASH_FUNC_START();
+	uint32_t _res;
+	
+	_res =  toeplitz_cache[0][_faddr & 0xff];
+	_res ^= toeplitz_cache[0][(_faddr >> 16) & 0xff];
+	_res ^= toeplitz_cache[0][_laddr & 0xff];
+	_res ^= toeplitz_cache[0][(_laddr >> 16) & 0xff];
+	_res ^= toeplitz_cache[0][_fport & 0xff];
+	_res ^= toeplitz_cache[0][_lport & 0xff];
+	
+	_res ^= toeplitz_cache[1][(_faddr >> 8) & 0xff];
+	_res ^= toeplitz_cache[1][(_faddr >> 24) & 0xff];
+	_res ^= toeplitz_cache[1][(_laddr >> 8) & 0xff];
+	_res ^= toeplitz_cache[1][(_laddr >> 24) & 0xff];
+	_res ^= toeplitz_cache[1][(_fport >> 8) & 0xff];
+	_res ^= toeplitz_cache[1][(_lport >> 8) & 0xff];
+
+	TRACE_PKTHASH_FUNC_END();
+	return _res;
+}
+/*---------------------------------------------------------------------*/
+static __inline uint32_t __unused
+toeplitz_rawhash_addr(in_addr_t _faddr, in_addr_t _laddr)
+{
+	TRACE_PKTHASH_FUNC_START();
+	uint32_t _res;
+	
+	_res =  toeplitz_cache[0][_faddr & 0xff];
+	_res ^= toeplitz_cache[0][(_faddr >> 16) & 0xff];
+	_res ^= toeplitz_cache[0][_laddr & 0xff];
+	_res ^= toeplitz_cache[0][(_laddr >> 16) & 0xff];
+	
+	_res ^= toeplitz_cache[1][(_faddr >> 8) & 0xff];
+	_res ^= toeplitz_cache[1][(_faddr >> 24) & 0xff];
+	_res ^= toeplitz_cache[1][(_laddr >> 8) & 0xff];
+	_res ^= toeplitz_cache[1][(_laddr >> 24) & 0xff];
+
+	TRACE_PKTHASH_FUNC_END();
+	return _res;
+}
+/*---------------------------------------------------------------------*/
+#if 0
+static __inline int
+toeplitz_hash(uint32_t _rawhash)
+{
+	TRACE_PKTHASH_FUNC_START();
+	TRACE_PKTHASH_FUNC_END();
+	return (_rawhash & 0xffff);
+}
+#endif
+/*---------------------------------------------------------------------*/
+static void
+toeplitz_cache_create(uint32_t cache[][256], int cache_len,
+		      const uint8_t key_str[], int key_strlen __unused)
+{
+	TRACE_PKTHASH_FUNC_START();
+	int i;
+	
+	for (i = 0; i < cache_len; ++i) {
+		uint32_t key[NBBY];
+		int j, b, shift, val;
+		
+		memset(key, 0, sizeof(key));
+		
+		/*
+		 * Calculate 32bit keys for one byte; one key for each bit.
+		 */
+		for (b = 0; b < NBBY; ++b) {
+			for (j = 0; j < 32; ++j) {
+				uint8_t k;
+				int bit;
+				
+				bit = (i * NBBY) + b + j;
+				
+				k = key_str[bit / NBBY];
+				shift = NBBY - (bit % NBBY) - 1;
+				if (k & (1 << shift))
+					key[b] |= 1 << (31 - j);
+			}
+		}
+		
+		/*
+		 * Cache the results of all possible bit combination of
+		 * one byte.
+		 */
+		for (val = 0; val < 256; ++val) {
+			uint32_t res = 0;
+			
+			for (b = 0; b < NBBY; ++b) {
+				shift = NBBY - b - 1;
+				if (val & (1 << shift))
+					res ^= key[b];
+			}
+			cache[i][val] = res;
+		}
+	}
+	TRACE_PKTHASH_FUNC_END();
+}
+/*---------------------------------------------------------------------*/
+#ifdef RSS_DEBUG  
+static void
+toeplitz_verify(void)
+{
+	TRACE_PKTHASH_FUNC_START();
+	in_addr_t faddr, laddr;
+	in_port_t fport, lport;
+	
+	/*
+	 * The first IPv4 example in the verification suite
+	 */
+	
+	/* 66.9.149.187:2794 */
+	faddr = 0xbb950942;
+	fport = 0xea0a;
+	
+	/* 161.142.100.80:1766 */
+	laddr = 0x50648ea1;
+	lport = 0xe606;
+	
+	kprintf("toeplitz: verify addr/port 0x%08x, addr 0x%08x\n",
+		toeplitz_rawhash_addrport(faddr, laddr, fport, lport),
+		toeplitz_rawhash_addr(faddr, laddr));
+	TRACE_PKTHASH_FUNC_END();
+}
+#endif  /* RSS_DEBUG */
+/*---------------------------------------------------------------------*/
+void
+toeplitz_get_key(uint8_t *key, int keylen)
+{
+	TRACE_PKTHASH_FUNC_START();
+	int i;
+	
+	if (keylen > TOEPLITZ_KEYLEN_MAX) {
+		TRACE_ERR("invalid key length %d", keylen);
+		TRACE_PKTHASH_FUNC_END();
+		exit(EXIT_FAILURE);
+	}
+	
+	/* Replicate key seeds to form key */
+	for (i = 0; i < keylen; ++i)
+		key[i] = toeplitz_keyseeds[i % TOEPLITZ_KEYSEED_CNT];
+	TRACE_PKTHASH_FUNC_END();
+}
+/*---------------------------------------------------------------------*/
+static void
+toeplitz_init(void *dummy __unused)
+{
+	TRACE_PKTHASH_FUNC_START();
+	uint8_t key[TOEPLITZ_INIT_KEYLEN];
+	int i;
+
+	for (i = 0; i < TOEPLITZ_KEYSEED_CNT; ++i)
+		toeplitz_keyseeds[i] &= 0xff;
+	
+	toeplitz_get_key(key, TOEPLITZ_INIT_KEYLEN);
+	
+#ifdef RSS_DEBUG
+	kprintf("toeplitz: keystr ");
+	for (i = 0; i < TOEPLITZ_INIT_KEYLEN; ++i)
+		kprintf("%02x ", key[i]);
+	kprintf("\n");
+#endif
+	toeplitz_cache_create(toeplitz_cache, TOEPLITZ_KEYSEED_CNT,
+			      key, TOEPLITZ_INIT_KEYLEN);
+	
+#ifdef RSS_DEBUG
+	toeplitz_verify();
+#endif
+	TRACE_PKTHASH_FUNC_END();
+}
 /*---------------------------------------------------------------------*/
 /**
  * The cache table is used to pick a nice seed for the hash value. It is
@@ -87,7 +277,7 @@ build_sym_key_cache(uint32_t *cache, int cache_len)
 /**
  * Computes symmetric hash based on the 4-tuple header data
  */
-static uint32_t
+static uint32_t __unused
 sym_hash_fn(uint32_t sip, uint32_t dip, uint16_t sp, uint32_t dp)
 {
 #define MSB32				0x80000000
@@ -102,6 +292,7 @@ sym_hash_fn(uint32_t sip, uint32_t dip, uint16_t sp, uint32_t dp)
 	
 	if (first_time) {
 		build_sym_key_cache(key_cache, KEY_CACHE_LEN);
+		toeplitz_init(NULL);
 		first_time = 0;
 	}
 	
@@ -140,10 +331,15 @@ decode_ip_n_hash(struct ip *iph, uint8_t hash_split, uint8_t seed)
 	uint32_t rc = 0;
 	
 	if (hash_split == 2) {
+#if 1
 		rc = sym_hash_fn(ntohl(iph->ip_src.s_addr),
 			ntohl(iph->ip_dst.s_addr),
 			ntohs(0xFFFD) + seed,
 			ntohs(0xFFFE) + seed);
+#else
+		rc = toeplitz_rawhash_addr(ntohl(iph->ip_src.s_addr),
+					   ntohl(iph->ip_dst.s_addr));
+#endif
 	} else {
 		struct tcphdr *tcph = NULL;
 		struct udphdr *udph = NULL;
@@ -151,17 +347,31 @@ decode_ip_n_hash(struct ip *iph, uint8_t hash_split, uint8_t seed)
 		switch (iph->ip_p) {
 		case IPPROTO_TCP:
 			tcph = (struct tcphdr *)((uint8_t *)iph + (iph->ip_hl<<2));
+#if 1
 			rc = sym_hash_fn(ntohl(iph->ip_src.s_addr), 
 					 ntohl(iph->ip_dst.s_addr), 
 					 ntohs(tcph->th_sport) + seed, 
 					 ntohs(tcph->th_dport) + seed);
+#else
+			rc = toeplitz_rawhash_addrport(ntohl(iph->ip_src.s_addr), 
+						       ntohl(iph->ip_dst.s_addr), 
+						       ntohs(tcph->th_sport) + seed, 
+						       ntohs(tcph->th_dport) + seed);
+#endif
 			break;
 		case IPPROTO_UDP:
 			udph = (struct udphdr *)((uint8_t *)iph + (iph->ip_hl<<2));
+#if 1
 			rc = sym_hash_fn(ntohl(iph->ip_src.s_addr),
 					 ntohl(iph->ip_dst.s_addr),
 					 ntohs(udph->uh_sport) + seed,
 					 ntohs(udph->uh_dport) + seed);
+#else
+			rc = toeplitz_rawhash_addrport(ntohl(iph->ip_src.s_addr),
+						       ntohl(iph->ip_dst.s_addr),
+						       ntohs(udph->uh_sport) + seed,
+						       ntohs(udph->uh_dport) + seed);	
+#endif
 			break;
 		case IPPROTO_IPIP:
 			/* tunneling */
@@ -173,10 +383,15 @@ decode_ip_n_hash(struct ip *iph, uint8_t hash_split, uint8_t seed)
 			 * the hash strength (although weaker but) should still hold 
 			 * even with 2 fields 
 			 */
+#if 1
 			rc = sym_hash_fn(ntohl(iph->ip_src.s_addr),
 					 ntohl(iph->ip_dst.s_addr),
 					 ntohs(0xFFFD) + seed,
 					 ntohs(0xFFFE) + seed);
+#else
+			rc = toeplitz_rawhash_addr(ntohl(iph->ip_src.s_addr),
+						   ntohl(iph->ip_dst.s_addr));
+#endif
 			break;
 		}
 	}
@@ -205,10 +420,15 @@ decode_ipv6_n_hash(struct ip6_hdr *ipv6h, uint8_t hash_split, uint8_t seed)
 		(ipv6h->ip6_dst.s6_addr[3] << 24);
 	
 	if (hash_split == 2) {
+#if 1
 		rc = sym_hash_fn(ntohl(saddr),
 				 ntohl(daddr),
 				 ntohs(0xFFFD) + seed,
 				 ntohs(0xFFFE) + seed);
+#else
+		rc = toeplitz_rawhash_addr(ntohl(saddr),
+					   ntohl(daddr));
+#endif
 	} else {
 		struct tcphdr *tcph = NULL;
 		struct udphdr *udph = NULL;
@@ -216,17 +436,31 @@ decode_ipv6_n_hash(struct ip6_hdr *ipv6h, uint8_t hash_split, uint8_t seed)
 		switch(ntohs(ipv6h->ip6_ctlun.ip6_un1.ip6_un1_nxt)) {
 		case IPPROTO_TCP:
 			tcph = (struct tcphdr *)(ipv6h + 1);
+#if 1
 			rc = sym_hash_fn(ntohl(saddr), 
 					 ntohl(daddr), 
 					 ntohs(tcph->th_sport) + seed, 
-					 ntohs(tcph->th_dport) + seed);	       
+					 ntohs(tcph->th_dport) + seed);
+#else
+			rc = toeplitz_rawhash_addrport(ntohl(saddr), 
+						       ntohl(daddr), 
+						       ntohs(tcph->th_sport) + seed, 
+						       ntohs(tcph->th_dport) + seed);
+#endif
 			break;
 		case IPPROTO_UDP:
 			udph = (struct udphdr *)(ipv6h + 1);
+#if 1
 			rc = sym_hash_fn(ntohl(saddr),
 					 ntohl(daddr),
 					 ntohs(udph->uh_sport) + seed,
-					 ntohs(udph->uh_dport) + seed);		
+					 ntohs(udph->uh_dport) + seed);
+#else
+			rc = toeplitz_rawhash_addrport(ntohl(saddr),
+						       ntohl(daddr),
+						       ntohs(udph->uh_sport) + seed,
+						       ntohs(udph->uh_dport) + seed);
+#endif
 			break;
 		case IPPROTO_IPIP:
 			/* tunneling */
@@ -248,10 +482,15 @@ decode_ipv6_n_hash(struct ip6_hdr *ipv6h, uint8_t hash_split, uint8_t seed)
 			 * the hash strength (although weaker but) should still hold 
 			 * even with 2 fields 
 			 */
+#if 1
 			rc = sym_hash_fn(ntohl(saddr),
 					 ntohl(daddr),
 					 ntohs(0xFFFD) + seed,
 					 ntohs(0xFFFE) + seed);
+#else
+			rc = toeplitz_rawhash_addr(ntohl(saddr),
+						   ntohl(daddr));			
+#endif
 		}
 	}
 	TRACE_PKTHASH_FUNC_END();
@@ -263,7 +502,7 @@ decode_ipv6_n_hash(struct ip6_hdr *ipv6h, uint8_t hash_split, uint8_t seed)
  * (See decode_vlan_n_hash & pkt_hdr_hash functions).
  */
 static uint32_t
-decode_others_n_hash(struct ether_header *ethh, uint8_t seed)
+decode_others_n_hash(struct ether_header *ethh, uint8_t seed __unused)
 {
 	TRACE_PKTHASH_FUNC_START();
 	uint32_t saddr, daddr, rc;
@@ -276,11 +515,16 @@ decode_others_n_hash(struct ether_header *ethh, uint8_t seed)
 		(ethh->ether_dhost[4] << 8) |
 		(ethh->ether_dhost[3] << 16) |
 		(ethh->ether_dhost[2] << 24);
-	
+
+#if 1
 	rc = sym_hash_fn(ntohl(saddr),
 			 ntohl(daddr),
 			 ntohs(0xFFFD) + seed,
 			 ntohs(0xFFFE) + seed);
+#else
+	rc = toeplitz_rawhash_addr(ntohl(saddr),
+				   ntohl(daddr));
+#endif
 
 	TRACE_PKTHASH_FUNC_END();
 	return rc;
